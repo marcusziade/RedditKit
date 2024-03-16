2e5e9c9595007920d1ae4e2677537e99cdcb1df7
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-03-16 09:15:15 -0700

    Update RedditKit package with new resources and targets

--- Sources/RedditKit/RedditKit.swift
+++ Sources/RedditKit/RedditKit.swift
@@ -1,2 +1,14 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+//let jsonFileName = "data.json"
+//if let jsonFileURL = Bundle.module.url(forResource: jsonFileName, withExtension: nil) {
+//    do {
+//        let jsonData = try Data(contentsOf: jsonFileURL)
+//        // Parse the JSON data and use it as needed
+//        // ...
+//    } catch {
+//        print("Error loading JSON file: \(error)")
+//    }
+//} else {
+//    print("JSON file not found")
+//}
