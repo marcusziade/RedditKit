eb15f076336b93a0921f6b67ade4684c69e1f1b4
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-03-16 09:25:13 -0700

    Add test case for loading JSON file in RedditKitTests

--- Tests/RedditKitTests/RedditKitTests.swift
+++ Tests/RedditKitTests/RedditKitTests.swift
@@ -1,12 +1,22 @@
 import XCTest
+
 @testable import RedditKit
 
 final class RedditKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testGetJSONSample() {
+        let jsonFileName = "r-final-fantasy-response.json"
+        if let jsonFileURL = Bundle.module.url(forResource: jsonFileName, withExtension: nil) {
+            do {
+                let jsonData = try Data(contentsOf: jsonFileURL)
+                let json = try JSONSerialization.jsonObject(with: jsonData, options: [])
+                XCTAssertNotNil(json)
+                XCTAssertTrue(json is [String: Any])
+            } catch {
+                print("Error loading JSON file: \(error)")
+            }
+        } else {
+            print("JSON file not found")
+        }
     }
 }
